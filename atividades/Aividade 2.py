d727682bf01424cb2ba36440bc0374173e1a7df1
Thiagojuy <thiagojuysg@gmail.com>
2026-09-01 22:01:53 -0300

    Atualizar o Aividade 2.py

--- atividades/Aividade 2.py
+++ atividades/Aividade 2.py
@@ -1,3 +1,19 @@
+
+
+"""
+
+ATIVIDADE 2 - OPERADORES
+Crie um algoritmo, que faça um formulário em que o usuário DIGITE seu nome, sua idade e se ele tem plano de saúde (True ou False)
+O eu sistema deve retornar em um único print(), todas as informações, e se ele for menor de idade ou idoso ou se não tiver plano de saúde, que ele não será aceito no nosso formulário;
+
+Exemplo de retorno no terminal: Seu nome é João, você tem 22 anos.
+Tem plano? False. Você foi aceito? False.
+
+
+""" 
+
+
+
 from operator import truediv
 
 nome = str(input("Digite seu nome:"))
