5d87df61c91ebd50b69928e9b7575a5ba7c5651c
thiagojuy <thiagojuysg@gmail.com>
2026-09-02 21:44:03 -0300

    Atividade 3

--- atividades/Atividade 3/Questão 7.py
+++ atividades/Atividade 3/Questão 7.py
@@ -6,4 +6,9 @@ ou igual a 16 anos, ter idade menor ou igual a 69 anos E pesar mais que 50kg.
 Escreva a lógica que verifica se o doador atende a todas essas condições e exiba
 na tela o resultado (True ou False).
 """
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite sua peso: "))
+doador = bool(idade >= 16 <= 69 or peso >= 50.0)
+input(bool("O doador preenche todos os requisitos para doar sangue?", doador))
+
 
