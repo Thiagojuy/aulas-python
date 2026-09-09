626550af53ef05f3080447f38b09eaebab4d3631
Thiagojuy <thiagojuysg@gmail.com>
2026-09-09 16:37:42 -0300

    Correct output message and clean up code
    Fix typo in output message and improve formatting.

--- atividades/Atividades 6/Questão 3.py
+++ atividades/Atividades 6/Questão 3.py
@@ -5,3 +5,14 @@ O programa deve continuar pedindo números até que o usuário digite o número 
 Quando o usuário digitar 0, o laço deve ser encerrado e o programa deve exibir a soma de
 todos os números que foram digitados até aquele momento.
 """
+
+
+soma = 0
+while True:
+    numero = int(input("Digite um numero inteiro: "))
+
+    if numero == 0:
+        break
+
+    soma += numero
+print(f"A soma de todos os mumeros digitas é", soma)
