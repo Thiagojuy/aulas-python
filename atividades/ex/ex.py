880c04dcfe0afa15485a1208c6584c62c5d94ec7
thiagojuy <thiagojuysg@gmail.com>
2026-09-10 21:37:26 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ atividades/ex/ex.py
@@ -0,0 +1,10 @@
+"""Crie uma lista que ela armazene um numero X de funcionários.
+Usando While, adicione quantos funcionários quiser.
+
+Com o for, voçê irá imprimir duas listas:
+Uma lista com todos os funcionários que receberão um aumento.
+Outra lista, com todos os funcionários que serçao demitidos.
+
+Voçê irá decidir qual funcionário será demitido ou receberá
+aumento pelo index do funcinário lista[]
+"""
